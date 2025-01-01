2bc47b807aa2ef7d7325cf9c85a1ed1afc78b171
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2025-01-01 20:25:59 +0530

    Added more functionality in project

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button"
+export default function Home(){
+return(
+<div>
+  <Button>Click Me</Button>
+</div>
+
+
+
+)
+
+}
+
+
+const SignupForm = () => {
+  return (
+    <div>SignupForm</div>
+  )
+}
+
+export default SignupForm
