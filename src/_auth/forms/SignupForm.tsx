84d0426c5efefd4c8535669d46e106c1e88e64dd
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2025-01-02 23:20:37 +0530

    minor upgrade

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button"
+
+  <Button>Click Me</Button>
+
+
+
+
+
+
+
+
+
+const SignupForm = () => {
+  return (
+    <div>
+<Button>Click Me</Button>
+
+    </div>
+  )
+}
+
+export default SignupForm
