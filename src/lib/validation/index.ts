21d024f9b90daeb47d7dd84fad8dadb341513a46
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2025-01-03 17:55:21 +0530

    a little mistake handled

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
  export const SignupValidation = z.object({
-    name: z.string().min(2{message: 'Too Short'}),
+    name: z.string().min(2,{message: 'Too Short'}),
     username: z.string().min(2).max(50),
   })
