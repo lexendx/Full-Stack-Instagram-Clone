b50aac08a5fe1df3588603fbd49350ebe9343ebb
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2025-01-06 18:02:40 +0530

    Sign up page

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,5 +2,7 @@ import { z } from "zod"
 
  export const SignupValidation = z.object({
     name: z.string().min(2,{message: 'Too Short'}),
-    username: z.string().min(2).max(50),
+    username: z.string().min(2,{ message: 'To Short'}),
+    email: z.string().email(),
+    password: z.string(). min(8,{ message: 'Password must be atleast 6 character.'}),
   })
